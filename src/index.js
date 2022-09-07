423c10145c2c1c538a9e01ed29d61f1c9c7d0597
QueGonJinn <molialow13@gmail.com>
2022-09-07 15:06:20 +0300

    feat: final changes

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import './index.html'; 
 import './index.scss';
+
+const counters = document.querySelectorAll('.skills_loading-percent'),
+      lines = document.querySelectorAll('.skills_loading-cl');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
+
+const menu = document.querySelector('.aside');
+hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger_active');
+    menu.classList.toggle('aside_active');
+});
